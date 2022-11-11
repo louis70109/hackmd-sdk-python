49f4d37120733ff465e1dfff3fc8b331a6615199
louis70109 <louis70109@gmail.com>
2022-11-11 21:42:44 +0800

    👣chore: update to header version

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -1,6 +1,7 @@
 import unittest
 import responses
 from hackmd.client import Hackmd
+from models.notes import NoteCreate, Note, NoteUpdate
 
 
 class TestClient(unittest.TestCase):
@@ -23,7 +24,7 @@ class TestClient(unittest.TestCase):
                     "owner_id": "00c437e8-9d79-4200-997f-8e9384415a76",
                     "path": "CAT",
                     "name": "API client testing team",
-                    "logo": "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+PHN2ZyB4bWxuczpyZGY9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkvMDIvMjItcmRmLXN5bnRheC1ucyMiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgaGVpZ2h0PSI5NiIgd2lkdGg9Ijk2IiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCA5NiA5NiI+PGc+PHJlY3Qgd2lkdGg9Ijk2IiBoZWlnaHQ9Ijk2IiBmaWxsPSIjZDg0ZGUyIiAvPjx0ZXh0IGZvbnQtc2l6ZT0iNjRweCIgZm9udC1mYW1pbHk9InNhbnMtc2VyaWYiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGZpbGw9IiNmZmZmZmYiPjx0c3BhbiB4PSI0OCIgeT0iNzIiIHN0cm9rZS13aWR0aD0iLjI2NDU4cHgiIGZpbGw9IiNmZmZmZmYiPkM8L3RzcGFuPjwvdGV4dD48L2c+PC9zdmc+",
+                    "logo": "data:image/svg+xml;base64,PD94bWwgdmVyc2lv...dmc+",
                     "description": "This is for testing client API",
                     "visibility": "public",
                     "created_at": 1644371278721
@@ -32,8 +33,9 @@ class TestClient(unittest.TestCase):
         }
 
         responses.add(
-            responses.GET,
-            f'{self.uri}/me',
+            method=responses.GET,
+            url=f'{self.uri}/me',
+            headers={'X-HackMD-API-Version': '1.0.0'},
             json=expect_response,
             status=200
         )
@@ -69,8 +71,9 @@ class TestClient(unittest.TestCase):
         }]
 
         responses.add(
-            responses.GET,
-            f'{self.uri}/notes',
+            method=responses.GET,
+            url=f'{self.uri}/notes',
+            headers={'X-HackMD-API-Version': '1.0.0'},
             json=expect_response,
             status=200
         )
@@ -81,7 +84,7 @@ class TestClient(unittest.TestCase):
 
     @responses.activate
     def test_get_note(self):
-        expect_response = {
+        expect_response = Note(**{
             "id": "ehgwc6a8RXSmcSaRwIQ2jw",
             "title": "Personal note title",
             "tags": [
@@ -106,15 +109,109 @@ class TestClient(unittest.TestCase):
             "read_permission": "guest",
             "write_permission": "signed_in",
             "publish_link": "https://hackmd.io/@username/permalink"
-        }
+        }).dict()
 
         responses.add(
-            responses.GET,
-            f'{self.uri}/note/ehgwc6a8RXSmcSaRwIQ2jw',
+            method=responses.GET,
+            url=f'{self.uri}/note/ehgwc6a8RXSmcSaRwIQ2jw',
+            headers={'X-HackMD-API-Version': '1.0.0'},
             json=expect_response,
             status=200
         )
         result = self.tested.get_note(note_id="ehgwc6a8RXSmcSaRwIQ2jw")
         request = responses.calls[0]
         self.assertEqual('GET', request.request.method)
+        self.assertEqual(result, expect_response)
+
+    @responses.activate
+    def test_create_note(self):
+        body = {
+            "title": "New note",
+            "content": "",
+            "read_permission": "owner",
+            "write_permission": "owner",
+            "comment_permission": "everyone"
+        }
+        expect_response = Note(**{
+            "id": "ehgwc6a8RXSmcSaRwIQ2jw",
+            "title": "New note",
+            "tags": [
+                "Personal",
+                "test"
+            ],
+            "created_at": 1643270371245,
+            "publish_type": "view",
+            "published_at": None,
+            "permalink": None,
+            "short_id": "SysJb0yAY",
+            "content": "# `test`",
+            "last_changed_at": 1644461594806,
+            "last_change_user": {
+                "name": "James",
+                "photo": "https://avatars.githubusercontent.com/u/26138990?s=96",
+                "biography": None,
+                "user_path": "AMQ36J15QgCZf46ThEFadg"
+            },
+            "user_path": "AMQ36J15QgCZf46ThEFadg",
+            "team_path": None,
+            "read_permission": "owner",
+            "write_permission": "owner",
+            "comment_permission": "everyone",
+            "publish_link": "https://hackmd.io/@username/permalink"
+        }).dict()
+
+        responses.add(
+            method=responses.POST,
+            url=f'{self.uri}/notes',
+            json=expect_response,
+            headers={'X-HackMD-API-Version': '1.0.0'},
+            status=201
+        )
+        result = self.tested.create_note(body=NoteCreate(**body))
+        request = responses.calls[0]
+        self.assertEqual('POST', request.request.method)
+        self.assertEqual(result, expect_response)
+
+    @responses.activate
+    def test_update_note(self):
+        body = {
+            "content": "# Updated personal note",
+            "read_permission": "signed_in",
+            "permalink": "note-permalink"
+        }
+        expect_response = Note(**{
+            "id": "ehgwc6a8RXSmcSaRwIQ2jw",
+            "title": "New note",
+            "tags": [],
+            "created_at": 1643270371245,
+            "publish_type": "view",
+            "published_at": None,
+            "permalink": "note-permalink",
+            "short_id": "SysJb0yAY",
+            "content": "# Updated personal note",
+            "last_changed_at": 1644461594806,
+            "last_change_user": {
+                "name": "James",
+                "photo": "https://avatars.githubusercontent.com/u/26138990?s=96",
+                "biography": None,
+                "user_path": "AMQ36J15QgCZf46ThEFadg"
+            },
+            "user_path": "AMQ36J15QgCZf46ThEFadg",
+            "team_path": None,
+            "read_permission": "signed_in",
+            "write_permission": "owner",
+            "comment_permission": "everyone",
+            "publish_link": "https://hackmd.io/@username/permalink"
+        }).dict()
+
+        responses.add(
+            method=responses.PATCH,
+            url=f'{self.uri}/notes/ehgwc6a8RXSmcSaRwIQ2jw',
+            headers={'X-HackMD-API-Version': '1.0.0'},
+            json=expect_response,
+            status=202
+        )
+        result = self.tested.update_note(note_id="ehgwc6a8RXSmcSaRwIQ2jw", body=NoteUpdate(**body))
+        request = responses.calls[0]
+        self.assertEqual('PATCH', request.request.method)
         self.assertEqual(result, expect_response)
