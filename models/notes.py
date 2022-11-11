c6a43d8db65fa425f37b41a329fe5e0e5714bc2a
louis70109 <louis70109@gmail.com>
2022-11-11 20:32:19 +0800

    ✨feat: add validator, NoteCreate and NoteUpdate model

--- models/notes.py
+++ models/notes.py
@@ -1,15 +1,12 @@
-import datetime
 from typing import List, Optional
-from pydantic import BaseModel
-from datetime import date
-
+from pydantic import BaseModel, validator
 from models.users import User
 
 
 class Notes(BaseModel):
     id: str
     title: str
-    tags: List[str]
+    tags: Optional[List[str]]
     created_at: int
     publish_type: str
     published_at: Optional[int] = None
@@ -23,6 +20,45 @@ class Notes(BaseModel):
     write_permission: str
     publish_link: str
 
+    @validator('read_permission', 'write_permission')
+    def permission_check(cls, v):
+        if v not in ['owner', 'signed_in', 'guest']:
+            raise ValueError("Permission must be ['owner', 'signed_in', 'guest']")
+        return v
 
 class Note(Notes):
     content: str
+
+
+class NoteCreate(BaseModel):
+    title: str
+    content: str
+    read_permission: str
+    write_permission: str
+    comment_permission: str
+
+    @validator('read_permission', 'write_permission')
+    def permission_check(cls, v):
+        if v not in ['owner', 'signed_in', 'guest']:
+            raise ValueError("Permission must be ['owner', 'signed_in', 'guest']")
+        return v
+
+    @validator('comment_permission')
+    def comment_permission_check(cls, v):
+        if v not in ['disabled', 'forbidden', 'owners', 'signed_in_users', 'everyone']:
+            raise ValueError(
+                "Comment permission must be ['disabled', 'forbidden', 'owners', 'signed_in_users', 'everyone']")
+        return v
+
+
+class NoteUpdate(BaseModel):
+    content: str
+    read_permission: str
+    write_permission: str
+    permalink: str
+
+    @validator('read_permission', 'write_permission')
+    def permission_check(cls, v):
+        if v not in ['owner', 'signed_in', 'guest']:
+            raise ValueError("Permission must be ['owner', 'signed_in', 'guest']")
+        return v
